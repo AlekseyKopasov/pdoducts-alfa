94e412da74ad3628548183c91134d718855b24ee
Алексей Копасов <alesey.k1@gmail.com>
2025-04-21 00:34:00 +0300

    обновляет deploy

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,8 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/pdoducts-alfa' : ''
+  basePath: process.env.NODE_ENV === 'production' ? '/pdoducts-alfa' : '',
+  trailingSlash: true
 };
 
 export default nextConfig;
